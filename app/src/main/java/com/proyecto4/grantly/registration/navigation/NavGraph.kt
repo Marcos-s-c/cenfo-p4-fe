36306251ba42141d368662dcf0dc895135cc4e33
squirosh <squirosh@ucenfotec.ac.cr>
2025-03-17 12:41:00 -0600

    Commit

--- app/src/main/java/com/proyecto4/grantly/registration/navigation/NavGraph.kt
+++ app/src/main/java/com/proyecto4/grantly/registration/navigation/NavGraph.kt
@@ -1,9 +1,11 @@
 package com.proyecto4.grantly.registration.navigation
 
+import ScholarshipCard.ScholarshipCard
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import com.proyecto4.grantly.bottomnavbar.BottomNavBar
 import com.proyecto4.grantly.registration.screens.RegistrationScreen
 import com.proyecto4.grantly.registration.screens.UserSelectionScreen
 import com.proyecto4.grantly.dashboard.screens.MainDashboardScreen
@@ -14,7 +16,7 @@ import com.proyecto4.grantly.dashboard.screens.MainDashboardScreen
 fun AppNavGraph(navController: NavHostController) {
     NavHost(
         navController = navController,
-        startDestination = "mainDashboard"
+        startDestination = "ScholarshipCard"
     ) {
         composable("userSelection") {
             UserSelectionScreen(navController = navController)
@@ -27,6 +29,12 @@ fun AppNavGraph(navController: NavHostController) {
         }
         composable("mainDashboard") {
             MainDashboardScreen()
+        }
+        composable("BottomNavBar") {
+            BottomNavBar()
+        }
+        composable("ScholarshipCard") {
+            ScholarshipCard()
         }
     }
 }
